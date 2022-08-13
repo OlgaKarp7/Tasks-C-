7b1d91009a2177b83a9fd69365620d74e1d78a09
OlgaKarpova <olialesha2006@mail.ru>
2022-08-13 22:58:11 +0300

    Решение задачи 6

---
+++ task 4/task 6/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число: ");
+Console.WriteLine("Введите второе число: ");
+Console.WriteLine("Введите третье число: ");
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+int numberC = int.Parse(Console.ReadLine());
+
+int max = numberA;
+
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+
+Console.Write("max = ");
+Console.WriteLine(max);
