6fc1652f9a0fc472d813db930b2efca98b6e9b0e
OlgaKarpova <olialesha2006@mail.ru>
2022-08-13 22:52:15 +0300

    Решение задачи 4

---
+++ task 4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int res = number%2;
+
+if (res == 0) 
+Console.WriteLine("ДА");
+
+else
+Console.WriteLine("нет");
