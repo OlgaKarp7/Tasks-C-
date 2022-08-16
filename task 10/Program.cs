f309151729db2f544e519530d947a610e181e44b
OlgaKarpova <olialesha2006@mail.ru>
2022-08-16 11:32:22 +0300

    Вторая цифра трехзначного  числа

--- task 10/Program.cs
+++ task 10/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число num ");
+
+int num = int.Parse(Console.ReadLine());
+int res = (num % 100) / 10;
+
+Console.Write($"Вторая цифра числа {num} это {res}");
