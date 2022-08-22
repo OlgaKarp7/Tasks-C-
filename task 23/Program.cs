43502ed322441928d56d43b9798b4207a84ae26c
OlgaKarpova <olialesha2006@mail.ru>
2022-08-23 00:19:42 +0300

    Исправили задачу 23

--- task 23/Program.cs
+++ task 23/Program.cs
@@ -4,9 +4,8 @@
 
 Console.WriteLine("Введите число N ");
 int N = int.Parse(Console.ReadLine());
-int i = 1;
-while ( i <= N )
+
+for ( int i = 1; i <= N; i++ )
 {
-    Console.WriteLine(i * i * i);
-    i++;
+    Console.Write(Math.Pow(i, 3)+" ");
 }    
