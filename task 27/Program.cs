f0d38e8b237ea85caf1d6549750c3619ed710eba
OlgaKarpova <olialesha2006@mail.ru>
2022-09-06 19:23:27 +0300

    Создали задачу 27

---
+++ task 27/Program.cs
@@ -0,0 +1,12 @@
+﻿/*  Напишите функцию и запустите ее, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+Console.WriteLine("Введите число number ");
+int number = int.Parse(Console.ReadLine());
+
+int SumCount(int a)
+{
+    int sum = 0;
+}
