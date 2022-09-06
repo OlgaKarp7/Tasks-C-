1149d2200cd2f36003f9563b7d234b0b973a48fb
OlgaKarpova <olialesha2006@mail.ru>
2022-09-06 17:57:26 +0300

    Создали задачу 25

--- task 25/Program.cs
+++ task 25/Program.cs
@@ -2,4 +2,20 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16  */
 
+Console.WriteLine("Введите числа a и b  ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
 
+int GetExpNum(int a)
+{
+    int res = 1;
+    
+    for (int i = 1; i <= b; i++)
+    {
+        res *= a;
+    }
+    return res;
+
+}
+
+Console.WriteLine(GetExpNum(a));
