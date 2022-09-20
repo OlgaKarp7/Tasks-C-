451926406bd3dae4be035c07db4f519a543bd46e
OlgaKarpova <olialesha2006@mail.ru>
2022-09-20 11:57:17 +0300

    Решили задачу 59

--- task54/Program.cs
+++ task54/Program.cs
@@ -8,4 +8,61 @@
 9 5 3 2
 8 4 4 2 */
 
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+
+            return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++) {
+        
+        for (int j = 0; j < array.GetLength(1); j++)
+           Console.Write($"{array[i, j]} ");
+        Console.WriteLine();   
+    }
+}
+
+void SortArrayRows(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++) 
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temporary = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temporary;
+                }
+            }
+        }
+}            
+
+
+Console.Write("m = ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("n = ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите первое число диапазона: ");
+int minValue = int.Parse(Console.ReadLine());
+Console.Write("Введите первое число диапазона: ");
+int maxValue = int.Parse(Console.ReadLine());
+
+
+int[,] myArray = GetArray(m, n, minValue, maxValue);
+PrintArray(myArray);
+Console.WriteLine();
+SortArrayRows(myArray);
+Console.WriteLine();
+PrintArray(myArray);
+
+
 
